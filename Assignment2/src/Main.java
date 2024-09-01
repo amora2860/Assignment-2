31dc36e182883e8be65810bd5dd3df971c80769e
amora <39012331+amora2860@users.noreply.github.com>
2024-08-31 22:12:00 -0400

    Refactored alot and added alot of comments.

--- Assignment2/src/Main.java
+++ Assignment2/src/Main.java
@@ -1,8 +1,5 @@
 
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,155 +9,164 @@ public class Main {
 
 
     public static void main(String[] args) {
-        /*
-        // Creating the instance with labeling for the frame and each button.
-        JFrame myFrame = new JFrame("Assignment #2");
-        JButton button1Display = new JButton("Display the text");
-        JButton button2Search = new JButton("Search");
-        JButton button7Exit = new JButton("Exit Program");
-        JLabel labelChoose = new JLabel("Please select an option.");
 
-        // Specifying the size of the frame.
-        myFrame.setSize(400, 250);
-
-        // Ensuring that absolute positioning is used for the buttons.
-        myFrame.setLayout(null);
-        myFrame.setVisible(true);
-
-        // Setting the absolute positions for all the buttons.
-        button1Display.setBounds(100, 50, 200, 40);
-        button2Search.setBounds(100, 100, 200, 40);
-        button7Exit.setBounds(100, 150, 200, 40);
-        labelChoose.setBounds(100, 10, 200, 40);
-
-        // Adding each button to the frame.
-        myFrame.add(button1Display);
-        myFrame.add(button2Search);
-        myFrame.add(button7Exit);
-        myFrame.add(labelChoose);
-
-        // Close program if X in the top right corner of the frame is clicked on.
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-*/
+        // This string of characters is not meant to be changed hence the final declaration.
         final String states = "Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada New Hampshire New Jersey New Mexico New York North Carolina North Dakota Ohio Oklahoma Oregon Pennsylvania Rhode Island South Carolina South Dakota Tennessee Texas Utah Vermont Virginia Washington West Virginia Wisconsin Wyoming";
-        System.out.println("Please enter your choice (1), (2) or (3).");
-        System.out.println("1 Display the text");
-        System.out.println("2 Search");
-        System.out.println("3 Exit program");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-//need a while loop that allows the menu to stay up when choices 1 , 2 are selected.
-        if(choice == 1) {
 
-            System.out.println(states);
-        }
-        if(choice == 2) {
-            System.out.println("Please enter the string of characters you would like to search for?");
-            String searchString = scanner.nextLine();
-            badCharacter(searchString, states);
-        }
-
-        if(choice == 3) {
-            System.exit(0);
-        }
-
-
-/*
-        // This button exits the program when clicked.
-        button7Exit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        // method to load the states characters into an array starting at i and ending at searchString.length(). (pass in i so starting point is correct)
-*/
-    }
-    public static void badCharacter(String input, String states)
-    {
-        List<Character> searchComparison = new ArrayList<>();
-        List<Integer> matchOutput = new ArrayList<>();
+        // This variable keeps track of where we are in states string.
         int i = 0;
-        // Ensuring that we go through the entirety of states string.
+
+        // Must ensure the menu always comes up after option 1 or 2 is selected.
+        while (true) {
+
+            System.out.println("Please enter 1, 2 or 3 to select an option below.");
+            System.out.println("1 Display the text");
+            System.out.println("2 Search");
+            System.out.println("3 Exit program");
+            try {
+                // Wait for user to into data.
+                Scanner scanner = new Scanner(System.in);
+
+                int choice = scanner.nextInt();
+                // If the user enters 1 then display states string.
+                if(choice == 1) {
+                    System.out.println(states);
+                }
+
+                // If the user enters 2 then ask for the searching string and begin comparing.
+                if(choice == 2) {
+                    System.out.println("Please enter the string of characters you would like to search for?");
+                    try {
+                        // Wait for user to into data.
+                        scanner = new Scanner(System.in);
+
+                        String searchInput = scanner.nextLine();
+
+                        boyerMooreAlgorithm(searchInput, states, i);
+                    }
+                    catch (Exception e){
+                        System.out.println("Please enter a valid string of character.");
+                    }
+                }
+                // If the user enters the number 3 end the program.
+                if(choice == 3) {
+                    System.out.println("Ending Program.");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Please enter either 1, 2 or 3.");
+                }
+            }
+            catch (Exception e){
+                System.out.println("Only numbers are accepted for a menu selection.");
+            }
+
+        }
+
+    }
+    // This method begins the process of
+    public static void boyerMooreAlgorithm(String searchInput, String states, int i)
+    {
+        // A list is needed to hold the characters being compared to.
+        List<Character> patternSearchComparisonList = new ArrayList<>();
+
+        //We need a list to contain all the matching indices.
+        List<Integer> matchingIndices = new ArrayList<>();
+
+
+        // Ensuring that we go through all the characters in states string.
         while (i < states.length()) {
-            // The loop must end since we won't be able to have a match since the characters left are too short to compare.
-            if ( (states.length() - i) < input.length())
+
+            // Ending the loop when the searching substring is too short in length to compare too.
+            if ( (states.length() - i) < searchInput.length())
             {
                 break;
             }
-            loadList(searchComparison, states, i, input.length());
-            i = checkMismatch(searchComparison, input, input.length(), i, matchOutput);
-            System.out.println(i);
-        }
-        System.out.println(matchOutput);
+            // Load into the patternSearchComparisonList characters from states
+            loadNewCharactersIntoComparisonList(patternSearchComparisonList, states, i, searchInput.length());
 
-        //textArea.append(matchOutput + "\n");
-        //JOptionPane.showMessageDialog(null, scrollPane, "List of indices where matches have been made.", JOptionPane.PLAIN_MESSAGE);
+            i = characterComparison(patternSearchComparisonList, searchInput, i, matchingIndices);
+
+        }
+
+        // Print out any results during the algorithm's operation.
+        printMatchingIndicesResults(matchingIndices);
+
     }
-    public static void loadList(List<Character> searchComparison, String states, int i, int length)
+
+    public static void loadNewCharactersIntoComparisonList(List<Character> patternSearchComparisonList, String states, int i, int searchInputLength)
     {
-        // We need to start with an empty list each time.
-        searchComparison.clear();
+        // We need to start with an empty list each time this method is called.
+        patternSearchComparisonList.clear();
+
         int m = 0;
 
-        while (m < length) {
-            //add each character from state(m) to searchComparison list (m-m).
-            searchComparison.add(states.charAt(m+i));
+        while (m < searchInputLength) {
+            //add each character from state string starting at i and moving to the length of .
+            patternSearchComparisonList.add(states.charAt(m+i));
             m++;
         }
     }
 
     // method checkMismatch to identify if there is a mismatch with each character of searchString starting from the right.
-    public static Integer checkMismatch(List<Character> searchComparison, String searchString, int searchStringLength, int i, List<Integer> matchOutput) {
-        // must check from right to left
-        int k = searchStringLength - 1;
-        while (k >= 0) {
-            // if we are at the end of the string of characters
+    public static Integer characterComparison(List<Character> searchComparison, String searchInput, int i, List<Integer> matchingIndices) {
 
+        int k = searchInput.length() - 1;
+        // We must compare the characters from right to left
+        while (k >= 0) {
 
             //check if each character does not match
-            if (searchComparison.get(k) != searchString.charAt(k))
+            if (searchComparison.get(k) != searchInput.charAt(k))
             {
-                // If we are at the end of the searchString and there is a non match then we need to move past the bad character.
-                if (k>=0){
-                    // Bad character has been found we must check if we need to move past it or if we have a match with it can we move to it.
-                    i = checkMatchSubstring( k, searchComparison, searchString, i);
-                    return i;
-                }
-                return i + searchStringLength;
+                // If we are at the end of the searchString and there is a non-match then we need to move past the bad character.
+                // Bad character has been found we must check if we need to move past it or if we have a match with it can we move to it.
+                i = checkBadcCharacterMatchesAnySubstringCharacters( k, searchComparison, searchInput, i);
+                return i;
             }
 
             // check if each character matches each other.
-            if (searchComparison.get(k) == searchString.charAt(k)) {
-                //k must be decremented so
+            if (searchComparison.get(k) == searchInput.charAt(k)) {
+                //k must be decrease so we can end the loop
                 k--;
             }
 
         }
             // add index of first character where match started to searchComparison list.
-            matchOutput.add(i);
-            return i + searchString.length();
+            matchingIndices.add(i);
+            return i + searchInput.length();
 
     }
     // The purpose is to see if the bad character matches any character in the substring or not.
-    public static Integer checkMatchSubstring (int k, List<Character> searchComparison, String searchString, int i)
+    public static Integer checkBadcCharacterMatchesAnySubstringCharacters (int k, List<Character> searchComparison, String searchInput, int i)
         {
             int j = k-1;
             // for each element of searchString we need to check against...
             while(j>=0)
             {
-             if (searchComparison.get(k) == searchString.charAt(j)){
+             if (searchComparison.get(k) == searchInput.charAt(j)){
                 // move i to where the bad character matches the character in searchString.
                  return i + (k-j);
                 }
                 j--;
             }
             // If the bad character does not exist in the substring then move past the mismatch.
-            //move past bad character
-
-                return i + (k+1);
-
+            return i + (k+1);
 
          }
+
+    public static void printMatchingIndicesResults(List<Integer> matchingIndices){
+
+        if (matchingIndices.isEmpty() ) {
+            System.out.println("No matches were found.");
+        }
+
+        else
+        {
+            System.out.println("The string searched on was found on the following indices.");
+            System.out.println(matchingIndices);
+        }
+
+    }
 
 }
 
